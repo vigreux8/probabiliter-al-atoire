a89ca6dc848faade71edc5b6e803e618e314795a
vigreux8 <vigreux8@hotmail.fr>
2023-04-10 22:29:18 +0200

    beug affiche la reponse

--- fonctions/fonction.py
+++ fonctions/fonction.py
@@ -28,7 +28,7 @@ class TypeProbabiliter(CalculculeMathematique):
         self.list_n_permutation_partiel = []
     
     def set_resultat(self,valeur):
-        self.resultat = valeur
+        self.resultat = int(valeur)
 
     def p_liste(self):
         self.model_selectionnner = "P_liste"
@@ -76,7 +76,8 @@ class CalculeProbabiliter(TypeProbabiliter):
         self.ordre = None
         self.model_selectionnner = None
         self.s = VariableObjetStreamlit()
-        self.reponse_utilisateur = None
+        self.reponse_utilisateur = True
+        self.bool_reponse = None
  
           
     def init_get_user_info(self):
@@ -135,8 +136,19 @@ class CalculeProbabiliter(TypeProbabiliter):
             self.ordre = False
             self.nb_tirage_p = 3
             self.cardinale_n = 9
-              
+    
+    def save_resultat(self):
+        if "reponse" not in st.session_state:
+            st.session_state.reponse =  int(self.resultat)      
 
+    def mise_a_jour_resultat(self):
+        st.session_state.reponse =  int(self.resultat)
+        st.session_state.model = self.model_selectionnner
+    
+    def affichage_reponse(self):
+        st.write(st.session_state.reponse)
+        
+    
     def get_resultat_et_choisis_liste(self):
         #p-liste  remise:True | ordre:True 
         #arrangement remise:False | ordre:True 
@@ -167,15 +179,16 @@ class CalculeProbabiliter(TypeProbabiliter):
             print("-le model choisi:",self.model_selectionnner,"\n-il y :",self.resultat,"possibilité.")
         elif ConstParamatreApplication.INTERFACE == ConstParamatreApplication.WEB:
             with self.s.colonne_centralle:
-                if not self.remise and not self.distingable :
-                    st.write("<p style='color: red; font-weight: bold;'>nombre d'élement ne peut être supérieur au tirage !</p>", unsafe_allow_html=True)
                 texte = f"-le model choisi: {self.model_selectionnner}"
                 texte_html = re.sub(f'({self.model_selectionnner})', r'<span style="background-color: blue;">\1</span>', texte)
                 st.write(texte_html, unsafe_allow_html=True)
                 st.write(f"-il y : {self.resultat} possibilité.")
                 if self.distingable =="p":
                     st.write(f"-la taille du groupe et de : {sum(self.list_n_permutation_partiel)}")
-                    
+    
+    def print_past_resultat(self):
+        st.session_state
+                 
     def print_explain_why(self):
         if ConstParamatreApplication.EXPLAIN_WHY:
             if self.distingable :
@@ -203,44 +216,61 @@ class CalculeProbabiliter(TypeProbabiliter):
             self.streamlit_choice_who_app_use()
         if not self.s.selecte_option_choice == ConstParamatreApplication.OPTION_CHOISI[2]:
             self.get_resultat_et_choisis_liste()
+            self.streamlit_avertissement()
             self.Print_explain_what_model()
             self.print_explain_why()
             self.print_resultat()
         else:
             self.selection_aléatoire()
             self.get_resultat_et_choisis_liste()
+            self.save_resultat()
             self.streamlit_affichage_detaile()
-            self.print_resultat()
+            self.get_reponse_utilisateur()
             
-            
-            
-            
+    def streamlit_avertissement(self):
+        with self.s.colonne_centralle:
+                if not self.remise and not self.distingable :
+                    st.write("<p style='color: red; font-weight: bold;'>nombre d'élement ne peut être supérieur au tirage !</p>", unsafe_allow_html=True)
             
     def streamlit_choice_who_app_use(self):
         with self.s.colonne_gauche:
             self.s.selecte_option_choice = st.selectbox("Options a choisir",ConstParamatreApplication.OPTION_CHOISI)
             if self.s.selecte_option_choice ==ConstParamatreApplication.OPTION_CHOISI[0]:
+                if "reponse" in st.session_state:
+                    self.del_save_varaible()
                 self.streamlit_app_distingable()
+                
             elif self.s.selecte_option_choice ==ConstParamatreApplication.OPTION_CHOISI[1]:
+                if "reponse" in st.session_state:
+                    self.del_save_varaible()
                 self.streamlit_app_ordre_and_tirage()
             elif self.s.selecte_option_choice ==ConstParamatreApplication.OPTION_CHOISI[2]:
                 self.Streamlit_app_entrainement()
                  
     def streamlit_affichage_detaile(self):
         with self.s.colonne_gauche:
-            st.write(f"remise : {self.converte_True_False_to_str(self.remise,'oui','non')}")
-            st.write(f"ordre : {self.converte_True_False_to_str(self.ordre,'oui','non')}")
-            st.write(f"distingable : {self.converte_True_False_to_str(self.distingable, si_vrais='t',si_faut='p',message_si_vrais= 'totalement',message_si_faut='partiellement')}")
-            st.write(f"nombre d'élément partiel : {len(self.list_n_permutation_partiel)}")
-            st.write(f"nombre de tirage :{self.nb_tirage_p}")
-            st.write(f"taille de la liste : {self.cardinale_n}")
-            st.write(f"nombre de personne par groupe : {self.list_n_permutation_partiel}")
-            
-
-            
-            
-            
+            if self.distingable == False:
+                st.write(f"remise : {self.converte_True_False_to_str(self.remise,'oui','non')}")
+                st.write(f"ordre : {self.converte_True_False_to_str(self.ordre,'oui','non')}")
+                st.write(f"distingable : non")
+                st.write(f"nombre de tirage : {self.nb_tirage_p}")
+                st.write(f"taille de la liste : {self.cardinale_n}")
+                
+            if self.distingable != False:
+                st.write(f"remise : non")
+                st.write(f"ordre :  non")
+                st.write(f"distingable : {self.converte_True_False_to_str(self.distingable, si_vrais='t',si_faut='p',message_si_vrais= 'totalement',message_si_faut='partiellement')}")
+                if self.distingable =="t":
+                    st.write(f"taille du groupe : {self.cardinale_n}")
+                # elif self.distingable =="p":
+                #     st.write(f"taille total : {sum(self.list_n_permutation_partiel)}")
                     
+            if self.distingable =="p":
+                st.write(f"nombre de groupe distingable: {len(self.list_n_permutation_partiel)}")
+                st.write(f"nombre de personne par groupe : {self.list_n_permutation_partiel}")  
+    
+    def del_save_varaible(self):
+        del st.session_state.reponse
     def streamlit_app_distingable(self):
         with self.s.colonne_gauche:
                 self.s.Select_box_distingable_type = st.selectbox("les objet sont distingable :",(f"{self.s.PARTILEMENT}",f"{self.s.TOTALEMENT}"))
@@ -278,10 +308,7 @@ class CalculeProbabiliter(TypeProbabiliter):
         """
         with self.s.colonne_centralle:
             st.write("Calculer le résultat :")
-            
-        with self.s.colonne_gauche:
-            pass
-        
+    
     def selection_aléatoire(self):
         """
         si la premier condition et respect on part sur :
@@ -295,7 +322,7 @@ class CalculeProbabiliter(TypeProbabiliter):
             print(self.remise)
             if not self.remise:
                 self.cardinale_n = random.randint(1,ConstParamatreApplication.HASART_MAXIMUM)
-                self.nb_tirage_p = random.randint(1,self.cardinale_n)
+                self.nb_tirage_p = random.randint(1,self.cardinale_n+1)
             else:
                 self.cardinale_n = random.randint(1,ConstParamatreApplication.HASART_MAXIMUM)
                 self.nb_tirage_p = random.randint(1,ConstParamatreApplication.HASART_MAXIMUM)
@@ -305,10 +332,34 @@ class CalculeProbabiliter(TypeProbabiliter):
             if self.distingable =="t":
                 self.cardinale_n = random.randint(1,ConstParamatreApplication.HASART_MAXIMUM)
             elif self.distingable =="p":
-                self.cardinale_n = random.randint(1,ConstParamatreApplication.HASART_MAXIMUM)
-                for i in range(1,random.randint(1,5)):
+                for i in range(0,random.randint(1,6)):
                     self.list_n_permutation_partiel.append(random.randint(1,5))
+                self.cardinale_n = sum(self.list_n_permutation_partiel)
 
+
+    
+    def get_reponse_utilisateur(self):
+        with self.s.colonne_centralle:
+            if self.reponse_utilisateur:
+                nombre = st.number_input("saisir le nombre de possibiliter",step=1,value=0)
+                if st.button("valider la réponse") :
+                    self.reponse_utilisateur = nombre
+                    print("reponse utilisateur : ",self.reponse_utilisateur )
+                    print("resultat : ", st.session_state.reponse)
+                    print("condition : ", self.reponse_utilisateur == st.session_state.reponse)
+                    if self.reponse_utilisateur == st.session_state.reponse:
+                            st.write("reponse correcte")
+                            self.mise_a_jour_resultat()
+                            self.print_resultat()
+                            
+                    else:
+                            st.write("reponse incorrect")
+                            self.mise_a_jour_resultat()
+                            self.print_resultat()
+                            
+
+        pass
+    
     @staticmethod
     def random_1_ou_2(si_vraie=True,si_faut=False):
         if random.randint(0,2) == 0:
